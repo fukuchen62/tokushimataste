aeaa4ec328c773941462fb485c5647019bbc2467
sasakiritsu <sasakidesu2024@gmail.com>
2024-11-21 07:54:04 +0900

    途中までのトップ
    トップページ途中まで
    
    佐々木

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
 // 自定義のJSの変数や関数などを記載してください。
+
+"use strict";
+//  ハンバーガーボタンがクリックされたら発動
+
+$(function () {
+    $('.hamburger').click(function () {
+        $('.hamburger, .slide-menu').toggleClass('active');
+    });
+});//scroll_effect
+
+
+// topにいくボタン
+
+$(function () {
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});

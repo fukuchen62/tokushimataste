7351b9c7fc148d0c29d6169a057829b07d482d20
q8suk <q8suklip@gmail.com>
2024-11-21 10:49:14 +0900

    テスト用のテンプレートパーツ作成
    テスト用のテンプレートパーツを作りました。
    
    香西

---
+++ test-template-parts-test/loop-test.php
@@ -0,0 +1,7 @@
+<div class="foodCard">
+    <a href="<?php the_permalink() ?>">
+        <div class="foodCard_body">
+            <h4 class="foodCard_title"><?php the_title() ?></h4>
+        </div>
+    </a>
+</div>

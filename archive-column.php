b22d6210c4d1a09d4b2d40c7114cd1a5bf457210
tsat-8 <kinoriri906389@gmail.com>
2024-11-22 23:39:37 +0900

    いったん中断
    foreachでボタン生成まではできました。タキソノミーコラムにつながらないので、いったん中断します。
    
    桑村

--- archive-column.php
+++ archive-column.php
@@ -1,6 +1,8 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" media="all">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css" media="all">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/column.css" media="all">
+
 <?php get_header(); ?>
-
-
 <main>
     <!-- コラム -->
     <section class="column">
@@ -16,46 +18,41 @@
                 <ul class="column_list">
                     <?php while (have_posts()) : the_post(); ?>
                         <li>
-                            <article>
-                                <div class="box_column">
-                                    <a href="#">
-                                        <div class="box__item">
-                                            <div>
-                                                <?php if (has_post_thumbnail()): ?>
-                                                    <?php the_post_thumbnail('medium'); ?>
-                                                <?php else: ?>
-                                                    <img src="<?php echo get_template_directory_uri(); ?>/test-assets-test/img/common/noimage.png" alt="">
-                                                <?php endif; ?>
-
-                                            </div>
-                                            <h3><?php the_title(); ?></h3>
-                                            <p>
-                                                <?php the_content(); ?>
-                                            </p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </article>
+                            <?php get_template_part('template-parts/loop', 'column'); ?>
                         </li>
                     <?php endwhile; ?>
                 </ul>
             <?php endif; ?>
-            <div>
-                <section>
-                    <div class="column_box">
-                        <h3>コラム　一覧</h3>
-                        <a href="#">インタビュー</a>
-                        <a href="#">体験談</a>
-                        <a href="#">取材日記</a>
-                    </div>
-                </section>
-            </div>
-            <!-- サービスのmoreボタン -->
-            <!-- <a href="#" class="more">サービス紹介</a> -->
+
         </div>
 
     </section>
+    <div>
+        <section>
+            <div class="column_box">
+                <h3>コラム　一覧</h3>
+                <?php
+                $column_type_terms = get_terms(['taxonomy' => 'column_type']);
+                // if (!empty($column_type_terms)):
 
+                // echo '<pre>';
+                // var_dump($column_type_terms);
+                // echo '<pre>';
+                ?>
+                <?php foreach ($column_type_terms as $column_type):
+                    // echo '<pre>';
+                    // var_dump($column_type);
+                    // echo '<pre>';
+                ?>
+                    <?php $new_link = get_term_link($column_type, 'taxonomy'); ?>
+                    <a href="<?php echo $new_link; ?>"><?php echo $column_type->name; ?></a>
+                <?php endforeach ?>
+                <!-- <a href="#">インタビュー</a>
+                <a href="#">体験談</a>
+                <a href="#">取材日記</a> -->
+            </div>
+        </section>
+    </div>
 </main>
 
 <?php get_footer(); ?>
---
+++ taxonomy-column.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" media="all">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css" media="all">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/column.css" media="all">
+
+<?php get_header(); ?>
+
+<body>
+
+
+    <!--パンくずリスト-->
+    <ul class="breadcrumb">
+        <ol itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="../html/index.html">
+                    <span itemprop="name">ホーム</span>
+                </a>
+                <meta itemprop="position" content="1" />
+            </li>
+
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="../html/column.html">
+                    <span itemprop="name">コラム</span>
+                </a>
+                <meta itemprop="position" content="2" />
+            </li>
+
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="../html/column_more.html">
+                    <span itemprop="name">インタビュー</span>
+                </a>
+                <meta itemprop="position" content="3" />
+            </li>
+        </ol>
+    </ul>
+
+
+    <!-- コラム -->
+    <section class="column">
+        <div class="inner">
+            <!-- 見出し -->
+            <h2 class="ttl_box">
+                <span class="ttl"><?php single_term_title('') ?></span><br>
+
+            </h2>
+
+            <!-- コラム一覧 -->
+            <?php if (have_posts()) : ?>
+                <ul class="column_list">
+                    <?php while (have_posts()) : the_post(); ?>
+                        <li>
+                            <?php get_template_part('template-parts/loop', 'column'); ?>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
+
+        </div>
+
+    </section>
+
+
+    </main>
+
+
+</body>
+
+<?php get_footer(); ?>

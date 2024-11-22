8e93f4fbc62f5d08029d4cf503832c07ed61cf3e
q8suk <q8suklip@gmail.com>
2024-11-22 10:38:41 +0900

    Update search.php
    少し修正しました
    
    香西

--- search.php
+++ search.php
@@ -14,32 +14,44 @@
             </div>
 
             <div class="section_body">
+                <ul class="foodList">
+                    <?php
+                    // メニューの投稿タイプ
+                    $args = [
+                        'post_type' => 'food',
+                        'post_per_page' => -1,
+                    ];
+                    $taxquerysp = ['relation' => 'AND'];
+                    $taxquerysp[] = [
+                        'taxonomy' => 'menu',
+                        'terms' => $menu->slug,
+                        'field' => 'slug',
+                    ];
+                    $args['tax_query'] = $taxqerysp;
+                    $the_query = new WP_Query($args);
+                    if ($the_query->have_posts()): ?>
+                        <div class="section_desc">
+                            <p><i class="fas fa-search"></i> 検索ワード「<?php the_search_query(); ?>」</p>
+                        </div>
 
+                        <div class="cardList">
+                            <?php while ($the_Query->have_posts()): $the_Query->the_post(); ?>
+                                <?php get_template_part('test-template-parts-test/loop', 'test'); ?>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata() ?>
+                        </div>
+                    <?php else: ?>
+                        <div class="section_desc">
+                            <p>検索結果はありませんでした</p>
+                        </div>
+                    <?php endif; ?>
 
-
-                <?php if ($the_query->have_posts()): ?>
-                    <div class="section_desc">
-                        <p><i class="fas fa-search"></i> 検索ワード「<?php the_search_query(); ?>」</p>
-                    </div>
-
-                    <div class="cardList">
-                        <?php while ($the_Query->have_posts()): $the_Query->the_post(); ?>
-                            <?php get_template_part('test-template-parts-test/loop', 'test'); ?>
-                        <?php endwhile; ?>
-                        <?php wp_reset_postdata() ?>
-                    </div>
-                <?php else: ?>
-                    <div class="section_desc">
-                        <p>検索結果はありませんでした</p>
-                    </div>
-                <?php endif; ?>
-
-                <?php if (function_exists('wp_pagenavi')): ?>
-                    <div class="pagination">
-                        <?php wp_pagenavi(); ?>
-                    </div>
-                <?php endif; ?>
-
+                    <?php if (function_exists('wp_pagenavi')): ?>
+                        <div class="pagination">
+                            <?php wp_pagenavi(); ?>
+                        </div>
+                    <?php endif; ?>
+                </ul>
             </div>
 
         </div>

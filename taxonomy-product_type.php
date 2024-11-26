db2b27c88732ad7f76b603b4cf81c95ce797b900
q8suk <q8suklip@gmail.com>
2024-11-26 16:34:03 +0900

    ファイル名を変更しました。
    タクソノミー名を表示しました。
    
    香西

--- taxonomy-product_type.php
+++ taxonomy-product_type.php
@@ -48,27 +48,7 @@
             <?php if (have_posts()): ?>
                 <?php while (have_posts()): the_post(); ?>
                     <li>
-                        <?php
-                        $pic = get_field('pic1');
-                        $pic_url = $pic['sizes']['large'];
-                        ?>
-                        <img src="<?php echo $pic_url; ?>" alt="Image" class="img-fluid"><br>
-                        <a href="<?php the_permalink(); ?>" id="goods"><?php the_title() ?></a>
-                        <?php
-                        $area_term = wp_get_object_terms(
-                            get_the_ID(),
-                            'area',
-                            array("fields" => "names")
-                        );
-                        $product_type_term = wp_get_object_terms(
-                            get_the_ID(),
-                            'product_type',
-                            array("fields" => "names")
-                        );
-                        ?>
-                        <p><?php echo $area_term[0];  ?></p>
-                        <p><?php echo $product_type_term[0];  ?></p>
-                        <p><?php the_field('introduction') ?></p>
+                        <?php get_template_part('template-parts/loop', 'product'); ?>
                     </li>
                 <?php endwhile; ?>
             <?php endif ?>

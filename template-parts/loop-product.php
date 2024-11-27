d6fe846947a221fd04d0fa641b6204f84c7a5246
q8suk <q8suklip@gmail.com>
2024-11-27 17:25:40 +0900

    頑張ってコメントを書きます。
    頑張ってコメントを書きます。
    
    香西

--- template-parts/loop-product.php
+++ template-parts/loop-product.php
@@ -5,6 +5,7 @@ $pic_url = $pic['sizes']['large'];
 <img src="<?php echo $pic_url; ?>" alt="Image" class="img-fluid"><br>
 <a href="<?php the_permalink(); ?>" id="goods"><?php the_title() ?></a>
 <?php
+// 個々の投稿のIDとタクソノミーを引数、フィールドを名前に設定している。
 $area_term = wp_get_object_terms(
     get_the_ID(),
     'area',
@@ -15,6 +16,9 @@ $product_type_term = wp_get_object_terms(
     'product_type',
     array("fields" => "names")
 );
+?>
+<?php
+// 返値は配列で帰ってくるので、配列の最初の要素を出力する。
 ?>
 <p><?php echo $area_term[0];  ?></p>
 <p><?php echo $product_type_term[0];  ?></p>

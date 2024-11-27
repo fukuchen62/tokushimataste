a8763d8733e7d6a7f610150b3b6ffd6ecc5ec010
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-11-27 22:33:43 +0900

    Update loop-maker.php
    メーカーループの中をテンプレート化
    
    山口

--- template-parts/loop-maker.php
+++ template-parts/loop-maker.php
@@ -1,32 +1,15 @@
-<a href="">
+<li>
     <div class="box_maker">
-        <?php
-        $pic = get_field('pic1');
-        $pic_url = $pic['sizes']['large'];
-        ?>
-        <img src="<?php echo $pic_url; ?>" alt="Image" class="img-fluid"><br>
-        <a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
-        <?php
-        $area_term = wp_get_object_terms(
-            get_the_ID(),
-            'maker',
-            array("fields" => "names")
-        );
-        $maker_type_term = wp_get_object_terms(
-            get_the_ID(),
-            'maker_type',
-            array("fields" => "names")
-        );
-        ?>
-        <p><?php echo $area_term[0];  ?></p>
-        <p><?php echo $maker_type_term[0];  ?></p>
-        <p><?php the_field('introduction') ?></p>
-</a>
-</li>
-
-
-<img src="../uploads/tennin (1).png" alt="メーカー写真">
-<h3>サブタイトルサブタイトル</h3>
-
-</a>
+        <a href="<?php the_permalink(); ?>">
+            <?php
+            $pic = get_field('pic1');
+            $pic_url = $pic ? $pic['sizes']['medium_large'] : '';
+            ?>
+            <?php if ($pic_url): ?>
+                <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+            <?php endif; ?>
+            <h3><?php the_title(); ?></h3>
+            <p><?php echo get_field('company_info'); ?></p>
+        </a>
+    </div>
 </li>

985ca030e67fad9ac52393b9e690487e31cf210d
q8suk <q8suklip@gmail.com>
2024-11-27 17:27:02 +0900

    途中経過
    先輩のファイルを参考にしながら頑張ります。
    
    香西

--- searchform.php
+++ searchform.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
-<main>
-    <!-- <form id="search-box" method="GET" action="<?php /*echo home_url('/')*/ ?>"> -->
-    <!-- wp検索フォームに必要 -->
-    <!-- <input type="hidden" name="s" value="">
+<!-- <main> -->
+<!-- <form id="search-box" method="GET" action="<?php /*echo home_url('/')*/ ?>"> -->
+<!-- wp検索フォームに必要 -->
+<!-- <input type="hidden" name="s" value="">
 
         <h2 class="detailed-search">
             <span class="detailed-search__text h2">
                 <i class="fas fa-search" aria-hidden="true"></i> 条件を組み合わせて検索
             </span>
         </h2> -->
-    <!-- 0601田邊修正 文字のスペース削除・クラス名変更など -->
-    <!-- <div class="table__wrap">
+<!-- 0601田邊修正 文字のスペース削除・クラス名変更など -->
+<!-- <div class="table__wrap">
             <table class="search-item select-box">
                 <tbody>
                     <tr>
@@ -19,12 +19,12 @@
                     </tr>
                     <tr class="search-nav"> -->
 
-    <!-- <td class="search-nav__box">
+<!-- <td class="search-nav__box">
                             <div class="search-nav__place">
                                 <label class="label-box"><input type="radio" class="place" name="area" value="tokushima" checked="">徳島市</label>
                             </div>
                         </td> -->
-    <!-- <td class="search-nav__box">
+<!-- <td class="search-nav__box">
                             <div class="search-nav__place">
                                 <label class="label-box"><input type="radio" class="place" name="area" value="east">東部</label>
                             </div>
@@ -152,128 +152,158 @@
             </table>
         </div> -->
 
-    <!-- 0523田邊変更 -->
-    <!-- 0527佐藤変更 他のページより差し替え -->
-    <!-- input[type="submit"] -->
-    <!-- <input type="submit" value="検索" class="btn-b ta-c"> -->
-    <!-- 0523田邊変更end -->
+<!-- 0523田邊変更 -->
+<!-- 0527佐藤変更 他のページより差し替え -->
+<!-- input[type="submit"] -->
+<!-- <input type="submit" value="検索" class="btn-b ta-c"> -->
+<!-- 0523田邊変更end -->
 
-    <!--
+<!--
         <div class="btn_box">
             <div class="js_btndown btn-link orange">
                 <button type="submit" class="btn-more__text">検索<i class="fas fa-angle-right"></i></button>
             </div>
         </div> -->
-    <!-- <input type="submit" value="検索" class="btn-b ta-c"> -->
-    <!-- 0601田邊修正 文字のスペース削除・クラス名変更などend -->
-    <!-- </form> -->
-    <!-- </main> -->
-    <main>
-        <!-- キービジュアル -->
-        <section class="kv b-shadow">
-            <div class="inner">
-                <h2 class="kv-copy">
-                    徳島の味！<br class="tb-none">
-                    あなたのご飯のお友達<br class="tb-none">
-                </h2>
-            </div>
-
-        </section>
-
-        <!-- 詳細検索 -->
-        <section class="search">
-
-            <!-- 見出し -->
-            <h2 class="ttl_box">
-                <span class="ttl">詳細検索</span><br>
+<!-- <input type="submit" value="検索" class="btn-b ta-c"> -->
+<!-- 0601田邊修正 文字のスペース削除・クラス名変更などend -->
+<!-- </form> -->
+<!-- </main> -->
+<main>
+    <!-- キービジュアル -->
+    <section class="kv b-shadow">
+        <div class="inner">
+            <h2 class="kv-copy">
+                徳島の味！<br class="tb-none">
+                あなたのご飯のお友達<br class="tb-none">
             </h2>
-            <div class="inner_search">
-                <table class="entryTable">
+        </div>
 
-                    <tr>
-                        <th>エリア</th>
-                        <td>
-                            <input type="checkbox" name="area" id="area1" value="県西" checked>
-                            <label for="area1">県西</label>
-                            <input type="checkbox" name="area" id="area2" value="県東">
-                            <label for="area2">県東</label>
-                            <input type="checkbox" name="area" id="area3" value="県南">
-                            <label for="area3">県南</label>
-                        </td>
-                    </tr>
-                    <tr>
+    </section>
 
-                    <tr>
-                        <th>お供のタイプ</th>
-                        <td>
-                            <input type="checkbox" name="otomoType" id="otomoType1" value="漬物・発酵食品">
-                            <label for="otomoType1">漬物・発酵食品</label>
-                            <input type="checkbox" name="otomoType" id="otomoType2" value="佃煮・海産物">
-                            <label for="otomoType2">佃煮・海産物</label>
-                            <input type="checkbox" name="otomoType" id="otomoType3" value="肉・卵・大豆製品">
-                            <label for="otomoType3">肉・卵・大豆製品</label>
-                            <input type="checkbox" name="otomoType" id="otomoType4" value="ふりかけ・混ぜご飯の素">
-                            <label for="otomoType4">ふりかけ・混ぜご飯の素</label>
-                            <input type="checkbox" name="otomoType" id="otomoType5" value="薬味・シンプル調味料">
-                            <label for="otomoType5">薬味・シンプル調味料</label>
-                            <input type="checkbox" name="otomoType" id="otomoType6" value="その他">
-                            <label for="otomoType6">その他</label>
-                        </td>
-                    </tr>
+    <!-- 詳細検索 -->
+    <form id="search-box" method="GET" action="<?php /*echo home_url('/')*/ ?>">
+        <!-- wp検索フォームに必要 -->
+        <input type="hidden" name="s" value="">
+        <section class="search">
+            <div class="inner">
+                <!-- 見出し -->
+                <h2 class="ttl_box">
+                    <span class="ttl">詳細検索</span><br>
+                </h2>
 
-                    <tr>
-                        <th>味覚</th>
-                        <td>
-                            <input type="checkbox" name="taste" id="taste1" value="甘い">
-                            <label for="taste1">甘い</label>
-                            <input type="checkbox" name="taste" id="taste2" value="辛い">
-                            <label for="taste2">辛い</label>
-                            <input type="checkbox" name="taste" id="taste3" value="塩辛い">
-                            <label for="taste3">塩辛い</label>
-                            <input type="checkbox" name="taste" id="taste4" value="苦い">
-                            <label for="taste4">苦い</label>
-                            <input type="checkbox" name="taste" id="taste5" value="うま味">
-                            <label for="taste5">うま味</label>
-                        </td>
-                    </tr>
+                <div class="check_box">
+                    <div section="inner_search">
+                        <ul>
+                            <li>
+                                <h3>エリア</h3>
+                            </li>
+                            <li><input type="checkbox" name="area" id="area1" value="県西" checked>
+                                <label for="area1">県西</label>
+                            </li>
+                            <li><input type="checkbox" name="area" id="area2" value="県東">
+                                <label for="area2">県東</label>
+                            </li>
+                            <li><input type="checkbox" name="area" id="area3" value="県南">
+                                <label for="area3">県南</label>
+                            </li>
+                        </ul>
+                    </div>
 
-                    <tr>
-                        <th>アレルギー</th>
-                        <td>
-                            <input type="checkbox" name="allergy" id="allergy1" value="えび">
-                            <label for="allergy1">えび</label>
-                            <input type="checkbox" name="allergy" id="allergy2" value="かに">
-                            <label for="allergy2">かに</label>
-                            <input type="checkbox" name="allergy" id="allergy3" value="クルミ">
-                            <label for="allergy3">くるみ</label>
-                            <input type="checkbox" name="allergy" id="allergy4" value="小麦">
-                            <label for="allergy4">小麦</label>
-                            <input type="checkbox" name="allergy" id="allergy5" value="そば">
-                            <label for="allergy5">そば</label>
-                            <input type="checkbox" name="allergy" id="allergy6" value="卵">
-                            <label for="allergy6">卵</label>
-                            <input type="checkbox" name="allergy" id="allergy7" value="乳">
-                            <label for="allergy7">乳</label>
-                            <input type="checkbox" name="allergy" id="allergy8" value="落花生">
-                            <label for="allergy8">落花生</label>
-                        </td>
-                    </tr>
+                    <div section="inner_search">
+                        <ul>
+                            <li>
+                                <h3>お供のタイプ</h3>
+                            </li>
+                            <li>
+                                <input type="checkbox" name="otomoType" id="otomoType1" value="漬物・発酵食品">
+                                <label for="otomoType1">漬物・発酵食品</label>
+                            </li>
+                            <li><input type="checkbox" name="otomoType" id="otomoType2" value="佃煮・海産物">
+                                <label for="otomoType2">佃煮・海産物</label>
+                            </li>
+                            <li><input type="checkbox" name="otomoType" id="otomoType3" value="肉・卵・大豆製品">
+                                <label for="otomoType3">肉・卵・大豆製品</label>
+                            </li>
+                            <li><input type="checkbox" name="otomoType" id="otomoType4" value="ふりかけ・混ぜご飯の素">
+                                <label for="otomoType4">ふりかけ・混ぜご飯の素</label>
+                            </li>
+                            <li><input type="checkbox" name="otomoType" id="otomoType5" value="薬味・シンプル調味料">
+                                <label for="otomoType5">薬味・シンプル調味料</label>
+                            </li>
+                            <li><input type="checkbox" name="otomoType" id="otomoType6" value="その他">
+                                <label for="otomoType6">その他</label>
+                            </li>
+                        </ul>
+                    </div>
 
+                    <div section="inner_search">
+                        <ul>
+                            <li>
+                                <h3>味覚</h3>
+                            </li>
+                            <li><input type="checkbox" name="taste" id="taste1" value="甘い">
+                                <label for="taste1">甘い</label>
+                            </li>
+                            <li><input type="checkbox" name="taste" id="taste2" value="辛い">
+                                <label for="taste2">辛い</label>
+                            </li>
+                            <li><input type="checkbox" name="taste" id="taste3" value="塩辛い">
+                                <label for="taste3">塩辛い</label>
+                            </li>
+                            <li><input type="checkbox" name="taste" id="taste4" value="苦い">
+                                <label for="taste4">苦い</label>
+                            </li>
+                            <li><input type="checkbox" name="taste" id="taste5" value="うま味">
+                                <label for="taste5">うま味</label>
+                            </li>
+                        </ul>
+                    </div>
 
-                </table>
+                    <div section="inner_search">
+                        <ul>
+                            <li>
+                                <h3>アレルギー</h3>
+                            </li>
 
-                <div class="entryBtns">
-                    <input type="reset" value="クリア">
-                    <input type="submit" value="検索">
+                            <li><input type="checkbox" name="allergy" id="allergy1" value="えび">
+                                <label for="allergy1">えび</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy2" value="かに">
+                                <label for="allergy2">かに</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy3" value="クルミ">
+                                <label for="allergy3">くるみ</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy4" value="小麦">
+                                <label for="allergy4">小麦</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy5" value="そば">
+                                <label for="allergy5">そば</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy6" value="卵">
+                                <label for="allergy6">卵</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy7" value="乳">
+                                <label for="allergy7">乳</label>
+                            </li>
+                            <li><input type="checkbox" name="allergy" id="allergy8" value="落花生">
+                                <label for="allergy8">落花生</label>
+                            </li>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="entryBtns">
+                        <input type="reset" value="クリア">
+                        <input type="submit" value="検索">
+                    </div>
                 </div>
             </div>
 
         </section>
-        </form>
-        <h2 class="ttl_box">
-            <span class="ttl">検索結果</span><br>
-        </h2>
-    </main>
-
-
-    <?php get_footer(); ?>
+    </form>
+    <h2 class="ttl_box">
+        <span class="ttl">検索結果</span><br>
+    </h2>
+</main>
+<?php get_footer(); ?>

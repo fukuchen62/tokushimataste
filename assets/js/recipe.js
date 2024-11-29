a4d4475b0b65f151a1766fcc67121b668991674c
tsat-8 <kinoriri906389@gmail.com>
2024-11-29 16:33:38 +0900

    recipe.js
    引っ越し
    
    桑村

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -144,3 +144,23 @@ $(document).ready(function () {
         }
     });
 });
+
+
+// レシピ詳細slick
+
+$(function () {
+    // メイン画像のオプション
+    $(".slider_ms").slick({
+        autoplay: false, // 自動再生ON
+        arrows: false, // 矢印非表示
+        asNavFor: ".thumbnail_ms", // サムネイルと同期
+
+    });
+    // サムネイルのオプション
+    $(".thumbnail_ms").slick({
+        slidesToShow: 3.5, // サムネイルの表示数
+        arrows: true, // 矢印表示
+        asNavFor: ".slider_ms", // メイン画像と同期
+        focusOnSelect: true, // サムネイルクリックを有効化
+    });
+});

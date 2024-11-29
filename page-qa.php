1b2d009e6fd604e07feb5e77e383d51177c071f2
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-11-29 12:07:38 +0900

    Create page-qa.php
    固定ページ作成
    
    山口

---
+++ page-qa.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="徳島を旅するように味わう">
+    <meta name="keywords" content="徳島,おとも,ご飯のおとも,徳島の味">
+    <title>Q&A</title>
+
+    <!--リセットCSS-->
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <!-- 共通CSSの読み込み -->
+    <link rel="stylesheet" href="../assets/css/common.css">
+
+    <!-- headerCSSの読み込み -->
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link href="../assets/css/top.css" rel="stylesheet">
+    <!-- footer CSSの読み込み -->
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    <!-- error CSSの読み込み -->
+    <link rel="stylesheet" href="../assets/css/qa.css">
+    <!--Googleフォントの読み込み-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Kiwi+Maru&family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+
+        <div class="kv">
+            <!-- <img src="../uploads/header-designkanpu.jpg" alt=""> -->
+
+            <p class="kv-copy">
+                    徳島の味！<br class="tb-none">
+                    あなたのご飯のお友達<br class="tb-none">
+            </p>
+        </div>
+
+        <div class="header-area pc-none">
+            <div class="title-logo">
+                <h1>
+                    <a href="#" class="logo">
+                        <img src="../uploads/miso.jpeg" alt="">
+                    </a>
+                </h1>
+            </div>
+
+            <div class="hamburger pc-none">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="h_nav sp-none ">
+                <ul class="slide-menu pc-none">
+                    <li><a href="goods.html">商品一覧</a></li>
+                    <li><a href="area.html.html">エリア検索</a></li>
+                    <li><a href="research.html">詳細検索</a></li>
+                    <li><a href="column.html.html">コラム</a></li>
+                    <li><a href="maker.html">メーカー一覧</a></li>
+                    <li><a href="recipe.html">アレンジレシピ</a></li>
+                    <li><a href="favorite.html">お気に入り</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- <div class="contentupper">
+                <form id="form1" action="ご飯のお供サイトURL">
+                    <input id="sbox1" name="s" type="text" placeholder="キーワードを入力" />
+                    <input id="sbtn1" type="submit" value="検索" />
+                </form>
+            </div> -->
+
+
+    <main>
+        <ul class="breadcrumb">
+            <ol itemscope itemtype="https://schema.org/BreadcrumbList">
+                <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                    <a itemprop="item" href="../html/index.html">
+                        <span itemprop="name">ホーム</span>
+                    </a>
+                    <meta itemprop="position" content="1" />
+                </li>
+
+                <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                    <a itemprop="item" href="#">
+                        <span itemprop="name">Q&A</span>
+                    </a>
+                    <meta itemprop="position" content="2" />
+                </li>
+        </ul>
+        </ol>
+        <br>
+        <div class="cp_qa01">
+            <dl>
+                <dt>ご飯のお供はどこで購入できますか？</dt>
+                <dd>ご飯のお供は、各公式サイトのホームページまたは
+                    スーパーやオンラインショップ、特産品を取り扱う店舗
+                    で購入することができます。
+                </dd>
+                <dt>掲載していただきたいのですが、費用はいくらかかりますか？</dt>
+                <dd>掲載費用はいただいておりません。その他の掲載に関してはお問い合わせページよりご連絡ください。</dd>
+                <dt>ご飯のお供の保存方法は？</dt>
+                <dd>ご飯のお供の保存方法は、冷蔵庫で保存するものや、常温で保存できるものがあります。開封後は早めに食べることをお勧めしますが、具体的な保存方法は商品ごとに異なる為、パッケージの指示を確認してください。</dd>
+                <dt>ご飯のお供の賞味期限はどのくらいですか？</dt>
+                <dd>ご飯の賞味期限は商品ごとによって異なりますが、
+                    一般的には数カ月から1年ほどです。開封後は早めに食べることを推薦します。</dd>
+                <dt>ご飯のお供のアレルギー情報はどこで確認できますか？</dt>
+                <dd>ご飯のお供のアレルギー情報は、サイト内の各商品詳細ページまたは
+                    商品のパッケージ、メーカーの公式ウェブサイトに記載されています。
+                    購入前に確認することをお勧めします。
+                </dd>
+                <dt>ご飯のお供の新商品情報はどこで確認できますか？</dt>
+                <dd>新商品情報は、メーカーの公式ウェブサイトやSNS、食品専門のニュースサイトなどで確認できます。また、店舗の新商品コーナーもチェックしてみてください。</dd>
+            </dl>
+            <br>
+        </div>
+        <div class="container"></div>
+        <h2 class="heading10" data-en="Dear Manufacturer"><span>掲載されているメーカー様へ</span></h2>
+        <p>電話番号やイベント内容等に変更がある場合は、お問い合わせフォームよりお問い合わせください。確認後、ご連絡させていただきます。</p>
+        <br>
+        <h2 class="heading10" data-en="Under Consideration"><span>本サイトに掲載をご検討中のメーカー様へ</h2>
+        <p>お問い合わせフォームよりお問い合わせください。確認後、ご連絡させていただきます。</p>
+        </div>
+        <br>
+        <ul class="flex">
+            <li>
+                <a href="../html/contact.html" class="original-button3">お問い合わせ</a>
+            </li>
+    </main>
+
+    <footer>
+        <div></div>
+
+
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>

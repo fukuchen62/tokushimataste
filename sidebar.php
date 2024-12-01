46813e46465727e696159e10c17f812ea05ef167
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-01 22:32:20 +0900

    サイドバー作り直し
    作り直し
    
    山口

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,36 @@
- <div>
- 	<section>
- 		<div class="column_box">
- 			<h3>
- 				<?php
-					$column_type_terms = get_terms(['taxonomy' => 'column_type']);
-					// if (!empty($column_type_terms)):
+    <?php echo 'これはサイドバーです。'; ?>
 
-					// echo '<pre>';
-					// var_dump($column_type_terms);
-					// echo '<pre>';
-					?></h3>
- 			<?php foreach ($column_type_terms as $column_type):
+    <aside class="side-menu">
+    	<ul class="side_bar side_bar_col">
+    		<li>
+    			<!-- サイドバーメニュー -->
+    			<div class="category-list-outer">
+    				<div class="category-list">
+    					<h3>コラム一覧</h3>
+    				</div>
+    			</div>
+    		</li>
+
+    		<li class="cat-item">
+    			<?php
+				$column_type_terms = get_terms(['taxonomy' => 'column_type']);
+				// if (!empty($column_type_terms)):
+
+				// echo '<pre>';
+				// var_dump($column_type_terms);
+				// echo '<pre>';
+				?>
+    			<?php foreach ($column_type_terms as $column_type):
 					// echo '<pre>';
 					// var_dump($column_type);
 					// echo '<pre>';
 				?>
- 				<li class="cat-item">
- 					<a href="<?php echo get_term_link($column_type); ?>"><?php echo $column_type->name; ?></a>
- 				<?php endforeach ?>
- 				<!-- <a href="#">インタビュー</a>
+    				<a href="<?php echo get_term_link($column_type); ?>"><?php echo $column_type->name; ?>
+    				</a>
+    			<?php endforeach ?>
+    			<!-- <a href="#">インタビュー</a>
                 <a href="#">体験談</a>
                 <a href="#">取材日記</a> -->
- 		</div>
- 	</section>
- </div>
+    		</li>
+    	</ul>
+    </aside>

c190cb19056d8a6d03d332c1c2b5dceb70397830
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-02 13:23:20 +0900

    Update loop-maker.php
    微調整
    
    山口

--- template-parts/loop-maker.php
+++ template-parts/loop-maker.php
@@ -1,6 +1,6 @@
-<li>
-    <div class="box_maker">
-        <a href="<?php the_permalink(); ?>">
+<div class="box_maker">
+    <a href="<?php the_permalink(); ?>">
+        <div class="box_intro">
             <?php
             $pic = get_field('pic1');
             $pic_url = $pic ? $pic['sizes']['medium_large'] : '';
@@ -12,23 +12,9 @@
             <p>住所:<?php the_field('address'); ?></p>
             <p>TEL:<?php the_field('tel'); ?></p>
             <p>営業時間：<?php the_field('business_hours'); ?></p>
-            <!-- <p><?php
-                    // 会社情報を取得
-                    //$company_info = get_field('company_info');
 
-                    // 会社情報が存在する場合、50文字に切り取って出力
-                    //if ($company_info) {
-                    //$trimmed_company_info = mb_substr($company_info, 0, 50, 'UTF-8'); // 50文字に切り取る
-                    //if (mb_strlen($company_info, 'UTF-8') > 50) {
-                    // $trimmed_company_info .= '...'; // 元の文字数が50を超えていた場合に "..." を追加
-                    //}
-                    //echo '<p>' . esc_html($trimmed_company_info) . '</p>';
-                    //}
-                    ?>
-            </p> -->
             <?php
 
             ?>
-        </a>
-    </div>
-</li>
+    </a>
+</div>

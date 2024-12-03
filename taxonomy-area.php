14a774ebe2168b11e2a8af881c148e9611386193
fukuchen62 <fukuchen62@gmail.com>
2024-12-03 17:30:16 +0900

    商品一覧とエリア一覧を仕上げました
    商品一覧とエリア一覧を仕上げました
    
    福島

--- taxonomy-area.php
+++ taxonomy-area.php
@@ -1,51 +1,63 @@
 <?php get_header() ?>
-<main>
-    <div class="inner">
-        <!-- パンくずリスト -->
-        <?php get_template_part('template-parts/breadcrumb'); ?>
 
-        <!-- エリア検索 -->
-        <h2 style="text-align: center;">お供の商品一覧表示</h2>
-        <section id="btn-area" class="wrap">
-            <!-- エリア検索ボタン -->
-            <ul class="btn-content">
-                <li id="east">
-                    <a href="<?php echo home_url('/area/east/'); ?>" class=""><span>県東</span></a>
+<main class="inner">
+    <!-- パンくずリスト -->
+    <?php get_template_part('template-parts/breadcrumb'); ?>
+
+    <!-- エリア検索 -->
+    <!-- <h2 style="text-align: center;">お供の商品一覧表示</h2> -->
+    <section id="btn-area" class="wrap">
+        <!-- エリア検索ボタン -->
+        <ul class="btn-content">
+            <?php $args = get_terms([
+                'taxonomy' => 'area',
+                'fields' => 'slugs',
+                // 投稿がないタクソノミーも表示させる
+                'hide_empty' => false,
+                // その他を最後に回す（wordpressの設定を使うことができました。）
+                "orderby" => "slug"
+            ]); ?>
+
+            <?php foreach ($args as $key => $value): ?>
+                <?php
+                // タクソノミーのタームを取得
+                $terms = get_terms(
+                    [
+                        'taxonomy' => 'area',
+                        'fields' => 'names',
+                        'slug' => $value,
+                        // 投稿がないタクソノミーも表示させる
+                        'hide_empty' => false
+                    ]
+                );
+                $term = get_term_by('slug', $value, 'area');
+                $url = get_term_link($term, 'area');
+                ?>
+                <li id="<?php print $value ?>">
+                    <a href="<?php echo $url; ?>" class="">
+                        <span><?php echo implode($terms); ?>
+                            <!-- ターム毎の投稿件数 -->
+                            (<?php echo $term->count; ?>)
+                        </span>
+                    </a>
                 </li>
-                <li id="west"><a href="<?php echo home_url('/area/west/'); ?>" class=""><span>県西</span></a>
-                </li>
-                <li id="south"><a href="<?php echo home_url('/area/south/'); ?>" class=""><span>県南</span></a>
-                </li>
-            </ul>
-        </section>
+            <?php endforeach; ?>
+        </ul>
+    </section>
 
+    <div class="card-container">
+        <ul class="page_list">
 
-        <?php //$area_slug = get_query_var('area');
-        ?>
-        <?php //var_dump($area_slug);
-        ?>
-        <?php //$area = get_term_by('slug', $area_slug, 'area');
-        ?>
-
-        <div class="card-container">
-            <ul class="page_list">
-
-                <?php if (have_posts()): ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <li>
-                            <?php get_template_part('template-parts/loop', 'product'); ?>
-                        </li>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </ul>
-        </div>
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <?php get_template_part('template-parts/loop', 'product'); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </ul>
+    </div>
+    <!-- ページネーション -->
+    <?php get_template_part('template-parts/pagination'); ?>
 
 </main>
-<?php
-get_template_part('template-parts/pagination');
-?>
+
 <?php get_footer(); ?>
-
-</body>
-
-</html>
--- taxonomy-product_type.php
+++ taxonomy-product_type.php
@@ -1,4 +1,5 @@
 <?php get_header() ?>
+
 <main class="inner">
     <?php get_template_part('template-parts/breadcrumb');
     ?>
@@ -8,8 +9,7 @@
     <section id="btn-area" class="wrap">
         <!-- ジャンル検索ボタン -->
         <ul class="btn-content">
-            <?php //商品種別のスラッグを全部配列に入れました。
-            ?>
+            <!-- 商品種別のスラッグを全部配列に入れました。 -->
             <?php $args = get_terms([
                 'taxonomy' => 'product_type',
                 'fields' => 'slugs',
@@ -21,36 +21,33 @@
 
             <?php foreach ($args as $key => $value): ?>
 
-            <?php
+                <?php
                 // タクソノミーのタームを取得
+                $terms = get_terms(
+                    [
+                        'taxonomy' => 'product_type',
+                        'fields' => 'names',
+                        'slug' => $value,
+                        // 投稿がないタクソノミーも表示させる
+                        'hide_empty' => false
+                    ]
+                );
+
+                // タームを取得
                 $term = get_term_by('slug', $value, 'product_type');
+                // urlを取得
+                $url = get_term_link($term);
                 ?>
-            <li id="<?php print $value ?>">
-                <a href="<?php echo get_term_link(
-                                    // タクソノミーのリンクを取得
-                                    get_term_by('slug', $value, 'product_type'),
-                                    // タクソノミーの中から、スラッグを検索している
-                                    'product_type'
-                                ) ?>" class="">
 
-                    <span><?php echo implode(
-                                    (get_terms(
-                                        [
-                                            'taxonomy' => 'product_type',
-                                            'fields' => 'names',
-                                            'slug' => $value,
-                                            // 投稿がないタクソノミーも表示させる
-                                            'hide_empty' => false
-                                        ]
-                                    )
-                                    )
-                                ) ?>
-                        <!-- ターム毎の投稿件数 -->
-                        (<?php echo $term->count; ?>)
-                    </span>
-                </a>
+                <li id="<?php print $value ?>">
+                    <a href="<?php echo $url; ?>" class="">
 
-            </li>
+                        <span><?php echo implode($terms); ?>
+                            <!-- ターム毎の投稿件数 -->
+                            (<?php echo $term->count; ?>)
+                        </span>
+                    </a>
+                </li>
             <?php endforeach; ?>
         </ul>
     </section>
@@ -60,9 +57,9 @@
         ?>
         <ul class="page_list">
             <?php if (have_posts()): ?>
-            <?php while (have_posts()): the_post(); ?>
-            <?php get_template_part('template-parts/loop', 'product'); ?>
-            <?php endwhile; ?>
+                <?php while (have_posts()): the_post(); ?>
+                    <?php get_template_part('template-parts/loop', 'product'); ?>
+                <?php endwhile; ?>
             <?php endif ?>
         </ul>
     </div>

5c702d5d36b528ee8c9d2779c4d90799185f2426
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-03 19:10:55 +0900

    Update single-column.php
    修正
    
    山口

--- single-column.php
+++ single-column.php
@@ -39,166 +39,40 @@
 
         <h3 class="related_all">関連する商品、<br>レシピ、メーカー様</h3>
 
-        <!-- 関連商品 -->
-        <p>商品（これはあとでけす）</p>
-        <section>
-            <ul class="good_used_flex">
-                <?php
-                $column_id = get_the_ID(); // 現在のコラム投稿のID
 
-                // column投稿のカスタムフィールド 'maker_id' を取得
-                $maker_id = get_field('maker_id', $column_id);
-
-                if ($maker_id) { // maker_id が存在する場合
-                    $args = [
-                        'post_type' => 'product',
-                        'meta_query' => [
-                            [
-                                'key' => 'maker_id', // 商品投稿のカスタムフィールド 'maker_id'
-                                'value' => $maker_id,
-                                'compare' => '=',
-                            ],
-                        ],
-                        'posts_per_page' => -1,
-                    ];
-
-                    $the_query = new WP_Query($args);
-
-                    if ($the_query->have_posts()):
-                        while ($the_query->have_posts()): $the_query->the_post(); ?>
-                            <li>
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="box_intro box_intro_col">
-                                        <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'thumbnail')); ?>" alt="<?php the_title(); ?>">
-
-                                        <h3 class="intro_sbtitle intro_sbtitle_cs">
-                                            <?php the_title(); ?>
-                                        </h3>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endwhile;
-                        wp_reset_postdata();
-                    else: ?>
-                        <p>関連する商品が見つかりませんでした。</p>
-                    <?php endif;
-                } else { ?>
-                    <p>関連するメーカーが登録されていません。</p>
-                <?php } ?>
-            </ul>
-        </section>
 
         <section>
             <ul class="good_used_flex">
                 <?php
-                $recipe_id = get_field('recipe_id'); // コラム投稿のカスタムフィールド 'recipe_id'
+                $id_list = get_field('recipe_id'); // コラム投稿のカスタムフィールド 'recipe_id'
+                ?>
+                <?php if (!empty($id_list)) : ?>
+                    <?php $id_list = explode(',', $id_list); ?>
+                    <?php foreach ($id_list as $key => $id) : ?>
 
-                if ($recipe_id) { // recipe_id が存在する場合
-                    $args = [
-                        'post_type' => 'column',
-                        'meta_query' => [
-                            [
-                                'key' => 'recipe_id', // 商品投稿のカスタムフィールド 'recipe_id'
-                                'value' => $recipe_id,
-                                'compare' => '=',
-                            ],
-                        ],
-                        'posts_per_page' => -1,
-                    ];
+                        <?php
 
-                    $the_query = new WP_Query($args);
+                        //id から投稿データを取得
+                        $ob_post = get_post($id);
 
-                    if ($the_query->have_posts()):
-                        while ($the_query->have_posts()): $the_query->the_post(); ?>
-                            <li>
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="box_intro box_intro_col">
-                                        <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'thumbnail')); ?>" alt="<?php the_title(); ?>">
+                        //print_r($ob_post);
 
-                                        <h3 class="intro_sbtitle intro_sbtitle_cs">
-                                            <?php the_title(); ?>
-                                        </h3>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endwhile;
-                        wp_reset_postdata();
-                    else: ?>
-                        <p>関連する商品が見つかりませんでした。</p>
-                    <?php endif;
-                } else { ?>
-                    <p>関連するレシピが登録されていません。</p>
-                <?php } ?>
+                        if ($ob_post) {
+                            //投稿タイトル (title) を取得
+                            $ob_title = $ob_post->post_title;
+
+                            //表示
+                            echo '<p>レシピ: ' . esc_html($ob_title) . '</p>';
+
+                            echo '<p>url: ' . esc_html(the_permalink($id)) . '</p>';
+                            // echo get_post_meta($id, 'address', true);
+                        }
+                        ?>
+
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </ul>
         </section>
-        <p>ここから</p>
-        <section>
-            <ul class="good_used_flex">
-                <?php
-                $column_id = get_the_ID(); // 現在のコラム投稿のID
-
-                // column投稿のカスタムフィールド 'recipe_id' を取得
-                $recipe_id = get_field('recipe_id', $column_id);
-
-                if ($recipe_id) { // recipe_id が存在する場合
-                    $args = [
-                        'post_type' => 'product',
-                        'meta_query' => [
-                            [
-                                'key' => 'recipe_id', // 商品投稿のカスタムフィールド 'recipe_id'
-                                'value' => $recipe_id,
-                                'compare' => '=',
-                            ],
-                        ],
-                        'posts_per_page' => -1,
-                    ];
-
-                    $the_query = new WP_Query($args);
-
-                    if ($the_query->have_posts()):
-                        while ($the_query->have_posts()): $the_query->the_post(); ?>
-                            <li>
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="box_intro box_intro_col">
-                                        <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'thumbnail')); ?>" alt="<?php the_title(); ?>">
-
-                                        <h3 class="intro_sbtitle intro_sbtitle_cs">
-                                            <?php the_title(); ?>
-                                        </h3>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endwhile;
-                        wp_reset_postdata();
-                    else: ?>
-                        <p>関連する商品が見つかりませんでした。</p>
-                    <?php endif;
-                } else { ?>
-                    <p>関連するレシピが登録されていません。</p>
-                <?php } ?>
-            </ul>
-        </section>
-
-
-        <p>ここまで</p>
-        <li>
-            <a href="../html/column_detail.html">
-                <div class="box_intro box_intro_col">
-                    <img src="../uploads/nikumisoitame.jpg" alt="コラムの写真">
-                    <h3 class="intro_sbtitle intro_sbtitle_cs">サブタイトルサブタイトル</h3>
-                </div>
-            </a>
-        </li>
-
-        </ul>
-        </section>
-
-        <p>実験</p>
-
-
-
-
-
 
 
 

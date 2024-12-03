0a1f5259edbc0f5f02973c4b8f3b7fec7e2d95cc
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-03 11:16:04 +0900

    マイページ更新
    マイページ更新
    css当たっていない
    
    山口

--- page-mypage.php
+++ page-mypage.php
@@ -5,248 +5,179 @@
     <div class="inner">
         <!-- パンくずリスト -->
         <?php get_template_part("template-parts/breadcrumb"); ?>
-
-        <?php
-
-
-        $spots = [];
-        // ブックマーク数カウント
-        $spot_count = 0;
-
-        ?>
-
-        <h2 style="text-align:center">お気に入りリスト</h2>
-
-        <!-- 伊丹さんのページについていたボタン必要なければ消す -->
-        <ul class="btn-content">
-            <li>
-                <a href="../html/goods.html" class=""><span>商品一覧ページへ</span></a>
-            </li>
-        </ul>
-
-
-
-        <div class="explanation explanation2">
+        <div class="explanation_fv">
             <p>本マイページはこのサイトにてご紹介した『商品』『アレンジレシピ』『コラム』を ブックマークし参照することができます。</p>
             <p>ブックマークにはCookieを使用しています。</p>
             <p>Cookieの削除に伴いブックマークした情報も削除されます。</p>
         </div>
 
-        <!-- 一つ目のカテゴリのカード群 -->
-        <!-- コンテンツのサブタイトル -->
-        <h2 class="sub__title2 mb20 bgGL">
-            <i class="fa-solid fa-paw"></i> お気に入りした<br>
-            商品一覧
-        </h2>
-        <div>
-            <?php
-            if (function_exists('get_user_favorites')) :
-                // code...
-                $favorites = get_user_favorites();
-                krsort($favorites);
+        <p>元HTML</p>
+        <section>
+            <div class="ttl_box_fv">
+                <h3>お気に入りにしたご飯のお供商品</h3>
+            </div>
+            <a href="">
+                <div class="favorite_box">
+                    <img src="<?php get_template_directory_uri(); ?>/uploads/tsukudani.jpeg" alt="Image" class="img-fluid">
+                    <p>海苔の佃煮パスタ</p>
+                </div>
+            </a>
+        </section>
+        <p>元HTMLここまで</p>
 
-                // print_r($favorites);
+        <!-- １つ目のカテゴリのカード群 -->
+        <section>
+            <div class="ttl_box_fv">
+                <h3>お気に入りにしたご飯のお供</h3>
+            </div>
 
-                if ($favorites) :
-                    $the_query = new WP_Query([
-                        'post_type' => 'product',
-                        'posts_per_page' => -1,
-                        'ignore_sticky_posts' => true,
-                        'post__in' => $favorites,
-                        'orderby' => 'post__in',
-                    ]); ?>
-                    <?php if ($the_query->have_posts()) : ?>
-                        <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
-                            <div class="container">
-                                <ul class="maker_list">
-                                    <li>
-                                        <!-- カード内情報 -->
-                                        <!-- サムネイルの取得 -->
-                                        <a href="<?php the_permalink(); ?>">
+            <div class="favorite_box">
+                <a href="<?php the_permalink(); ?>">
+                    <?php
+                    if (function_exists('get_user_favorites')) :
+                        // code...
+                        $favorites = get_user_favorites();
+                        krsort($favorites);
 
-                                        </a>
-                                        <div class="box_intro">
-                                            <?php if (has_post_thumbnail()) : ?>
-                                                <?php the_post_thumbnail("medium"); ?>
-                                            <?php else : ?>
-                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="">
-                                            <?php endif; ?>
-                                            <h3 class="intro_sbtitle"><?php the_field("product_name"); ?></h3>
-                                            <p><?php //the_field("");
-                                                ?></p>
-                                        </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        <?php
-                            $spot_count++;
-                        // end ループ while
-                        endwhile;
-                        wp_reset_postdata();
-                        // else :
-                        ?>
+                        // print_r($favorites);
 
+                        if ($favorites) :
+                            $the_query = new WP_Query([
+                                'post_type' => 'product',
+                                'posts_per_page' => -1,
+                                'ignore_sticky_posts' => true,
+                                'post__in' => $favorites,
+                                'orderby' => 'post__in',
+                            ]); ?>
+                            <?php if ($the_query->have_posts()) : ?>
+                                <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
+                                    <!-- カード内情報 -->
+                                    <?php
+                                    $pic = get_field('pic1');
+                                    $pic_url = $pic ? $pic['sizes']['medium_large'] : '';
+                                    ?>
+                                    <?php if ($pic_url): ?>
+                                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                                    <?php endif; ?>
+                                    <p><?php the_field("product_name"); ?>
+                                    </p>
+                                    <p><?php //the_field("");
+                                        ?></p>
 
-            <?php
-                    else :
-
+                                <?php
+                                // end ループ while
+                                endwhile;
+                                wp_reset_postdata();
+                                // else :
+                                ?>
+                    <?php
+                            endif;
+                        endif;
+                    // end favorites
                     endif;
-                endif;
-            // end favorites
-            endif;
+                    ?>
+                </a>
+            </div>
+        </section>
 
-            ?>
-        </div>
+        <!-- ２つ目のカテゴリのカード群 -->
+        <section>
+            <div class="ttl_box_fv">
+                <h3>お気に入りにしたコラム</h3>
+            </div>
+            <div class="favorite_box">
+                <a href="<?php the_permalink(); ?>">
+                    <?php
+                    if (function_exists('get_user_favorites')) :
+                        // code...
+                        $favorites = get_user_favorites();
+                        krsort($favorites);
 
+                        // print_r($favorites);
 
-
-        <!-- コンテンツのサブタイトル -->
-        <h2 class="sub__title2 mb20 bgBL">
-            <i class="fa-solid fa-paw"></i> お気に入りした<br>
-            コラム一覧
-        </h2>
-
-        <!-- <div class="next__info tr2"><a href="">>>〇〇件</a></div> -->
-
-        <!-- 2つめのカテゴリ -->
-        <div>
-            <?php
-            if (function_exists('get_user_favorites')) :
-                // code...
-                $favorites = get_user_favorites();
-                krsort($favorites);
-
-                // print_r($favorites);
-
-                if ($favorites) :
-                    $the_query = new WP_Query([
-                        'post_type' => 'column',
-                        'posts_per_page' => -1,
-                        'ignore_sticky_posts' => true,
-                        'post__in' => $favorites,
-                        'orderby' => 'post__in',
-                    ]); ?>
-                    <?php if ($the_query->have_posts()) : ?>
-                        <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
-                            <div class="container">
-                                <ul class="maker_list">
-                                    <li>
-                                        <!-- カード内情報 -->
-                                        <!-- サムネイルの取得 -->
-                                        <a href="<?php the_permalink(); ?>">
-
-                                        </a>
-                                        <div class="box_intro">
-                                            <?php if (has_post_thumbnail()) : ?>
-                                                <?php the_post_thumbnail("medium"); ?>
-                                            <?php else : ?>
-                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="">
-                                            <?php endif; ?>
-                                            <h3 class="intro_sbtitle"><?php the_title(); ?></h3>
-                                            <p><?php //the_field("");
-                                                ?></p>
-                                        </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        <?php
-                            $spot_count++;
-                        // end ループ while
-                        endwhile;
-                        wp_reset_postdata();
-                        // else :
-                        ?>
-
-
-            <?php
-                    else :
-
+                        if ($favorites) :
+                            $the_query = new WP_Query([
+                                'post_type' => 'column',
+                                'posts_per_page' => -1,
+                                'ignore_sticky_posts' => true,
+                                'post__in' => $favorites,
+                                'orderby' => 'post__in',
+                            ]); ?>
+                            <?php if ($the_query->have_posts()) : ?>
+                                <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
+                                    <?php if (has_post_thumbnail()) : ?>
+                                        <?php the_post_thumbnail("medium"); ?>
+                                    <?php else : ?>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="">
+                                    <?php endif; ?>
+                                    <p><?php the_title(); ?></p>
+                                    <p><?php //the_field("");
+                                        ?></p>
+                                <?php
+                                // end ループ while
+                                endwhile;
+                                wp_reset_postdata();
+                                // else :
+                                ?>
+                    <?php
+                            else :
+                            endif;
+                        endif;
+                    // end favorites
                     endif;
-                endif;
-            // end favorites
-            endif;
+                    ?>
+                </a>
+            </div>
+        </section>
 
-            ?>
-        </div>
+        <!-- ３つ目のカテゴリのカード群 -->
+        <section>
+            <div class="ttl_box_fv">
+                <h3>お気に入りにしたレシピ</h3>
+            </div>
+            <div class="favorite_box">
+                <a href="<?php the_permalink(); ?>">
+                    <?php
+                    if (function_exists('get_user_favorites')) :
+                        // code...
+                        $favorites = get_user_favorites();
+                        krsort($favorites);
 
+                        // print_r($favorites);
 
-        <!-- <button id="btn__east" class="more-button">
-        MORE
-    </button> -->
-
-        <!-- コンテンツのサブタイトル -->
-        <h2 class="sub__title2 mb20 bgPK"><i class="fa-solid fa-paw"></i> お気に入りした<br>アレンジレシピ</h2>
-
-        <div>
-            <?php
-            if (function_exists('get_user_favorites')) :
-                // code...
-                $favorites = get_user_favorites();
-                krsort($favorites);
-
-                // print_r($favorites);
-
-                if ($favorites) :
-                    $the_query = new WP_Query([
-                        'post_type' => 'recipe',
-                        'posts_per_page' => -1,
-                        'ignore_sticky_posts' => true,
-                        'post__in' => $favorites,
-                        'orderby' => 'post__in',
-                    ]); ?>
-                    <?php if ($the_query->have_posts()) : ?>
-                        <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
-                            <div class="container">
-                                <ul class="maker_list">
-                                    <li>
-                                        <!-- カード内情報 -->
-                                        <!-- サムネイルの取得 -->
-                                        <a href="<?php the_permalink(); ?>">
-
-                                        </a>
-                                        <div class="box_intro">
-                                            <?php if (has_post_thumbnail()) : ?>
-                                                <?php the_post_thumbnail("medium"); ?>
-                                            <?php else : ?>
-                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="">
-                                            <?php endif; ?>
-                                            <h3 class="intro_sbtitle"><?php the_field("recipe_name");
-                                                                        ?></h3>
-                                        </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        <?php
-                            $spot_count++;
-                        // end ループ while
-                        endwhile;
-                        wp_reset_postdata();
-                        // else :
-                        ?>
-
-
-            <?php
-                    else :
-
+                        if ($favorites) :
+                            $the_query = new WP_Query([
+                                'post_type' => 'recipe',
+                                'posts_per_page' => -1,
+                                'ignore_sticky_posts' => true,
+                                'post__in' => $favorites,
+                                'orderby' => 'post__in',
+                            ]); ?>
+                            <?php if ($the_query->have_posts()) : ?>
+                                <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
+                                    <?php if (has_post_thumbnail()) : ?>
+                                        <?php the_post_thumbnail("medium"); ?>
+                                    <?php else : ?>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="">
+                                    <?php endif; ?>
+                                    <p class="intro_sbtitle"><?php the_field("recipe_name");
+                                                                ?></p>
+                                <?php
+                                // end ループ while
+                                endwhile;
+                                wp_reset_postdata();
+                                // else :
+                                ?>
+                    <?php
+                            else :
+                            endif;
+                        endif;
+                    // end favorites
                     endif;
-                endif;
-            // end favorites
-            endif;
-
-            ?>
-        </div>
-
-
-        <!-- <button id="btn__west" class="more-button">
-        MORE
-    </button> -->
-
-
+                    ?>
+                </a>
+            </div>
+        </section>
     </div>
 </main>
 

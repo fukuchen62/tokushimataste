23320c0bae04dc3f08203fd53387a594ed0f559a
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-03 23:08:54 +0900

    singleコラム完成
    singleコラム完成
    
    山口

--- single-column.php
+++ single-column.php
@@ -39,42 +39,107 @@
 
         <h3 class="related_all">関連する商品、<br>レシピ、メーカー様</h3>
 
+        <!-- 商品 -->
+        <section>
+            <ul class="good_used_flex">
+                <?php
+                $id_list = get_field('product_id'); // コラム投稿のカスタムフィールド 'maker_id'
+                if (!empty($id_list)) :
+                    $id_list = explode(',', $id_list); // カンマ区切りを配列に変換
+                    foreach ($id_list as $key => $id) :
+                        $ob_post = get_post($id); // 投稿データを取得
+                        if ($ob_post) :
+                            $ob_title = $ob_post->post_title; // タイトルを取得
+                            $thumbnail = get_the_post_thumbnail($id, 'medium'); // サムネイルを取得
+                            $permalink = get_permalink($id); // 投稿のURLを取得
+                ?>
+                            <li>
+                                <a href="<?php echo esc_url($permalink); ?>">
+                                    <div class="box_intro box_intro_col">
+                                        <?php if ($thumbnail) : ?>
+                                            <img src="<?php echo esc_url(get_the_post_thumbnail_url($id, 'medium')); ?>" alt="<?php echo esc_attr($ob_title); ?>">
+                                        <?php else : ?>
+                                            <img src="../uploads/default-image.jpg" alt="デフォルト画像">
+                                        <?php endif; ?>
+                                        <h3 class="intro_sbtitle intro_sbtitle_cs"><?php echo esc_html($ob_title); ?></h3>
+                                    </div>
+                                </a>
+                            </li>
+                <?php
+                        endif;
+                    endforeach;
+                endif;
+                ?>
+            </ul>
+        </section>
 
-
+        <!-- レシピ -->
         <section>
             <ul class="good_used_flex">
                 <?php
                 $id_list = get_field('recipe_id'); // コラム投稿のカスタムフィールド 'recipe_id'
+                if (!empty($id_list)) :
+                    $id_list = explode(',', $id_list); // カンマ区切りを配列に変換
+                    foreach ($id_list as $key => $id) :
+                        $ob_post = get_post($id); // 投稿データを取得
+                        if ($ob_post) :
+                            $ob_title = $ob_post->post_title; // タイトルを取得
+                            $thumbnail = get_the_post_thumbnail($id, 'medium'); // サムネイルを取得
+                            $permalink = get_permalink($id); // 投稿のURLを取得
                 ?>
-                <?php if (!empty($id_list)) : ?>
-                    <?php $id_list = explode(',', $id_list); ?>
-                    <?php foreach ($id_list as $key => $id) : ?>
-
-                        <?php
-
-                        //id から投稿データを取得
-                        $ob_post = get_post($id);
-
-                        //print_r($ob_post);
-
-                        if ($ob_post) {
-                            //投稿タイトル (title) を取得
-                            $ob_title = $ob_post->post_title;
-
-                            //表示
-                            echo '<p>レシピ: ' . esc_html($ob_title) . '</p>';
-
-                            echo '<p>url: ' . esc_html(the_permalink($id)) . '</p>';
-                            // echo get_post_meta($id, 'address', true);
-                        }
-                        ?>
-
-                    <?php endforeach; ?>
-                <?php endif; ?>
+                            <li>
+                                <a href="<?php echo esc_url($permalink); ?>">
+                                    <div class="box_intro box_intro_col">
+                                        <?php if ($thumbnail) : ?>
+                                            <img src="<?php echo esc_url(get_the_post_thumbnail_url($id, 'medium')); ?>" alt="<?php echo esc_attr($ob_title); ?>">
+                                        <?php else : ?>
+                                            <img src="../uploads/default-image.jpg" alt="デフォルト画像">
+                                        <?php endif; ?>
+                                        <h3 class="intro_sbtitle intro_sbtitle_cs"><?php echo esc_html($ob_title); ?></h3>
+                                    </div>
+                                </a>
+                            </li>
+                <?php
+                        endif;
+                    endforeach;
+                endif;
+                ?>
             </ul>
         </section>
 
-
+        <!-- メーカー -->
+        <section>
+            <ul class="good_used_flex">
+                <?php
+                $id_list = get_field('maker_id'); // コラム投稿のカスタムフィールド 'maker_id'
+                if (!empty($id_list)) :
+                    $id_list = explode(',', $id_list); // カンマ区切りを配列に変換
+                    foreach ($id_list as $key => $id) :
+                        $ob_post = get_post($id); // 投稿データを取得
+                        if ($ob_post) :
+                            $ob_title = $ob_post->post_title; // タイトルを取得
+                            $thumbnail = get_the_post_thumbnail($id, 'medium'); // サムネイルを取得
+                            $permalink = get_permalink($id); // 投稿のURLを取得
+                ?>
+                            <li>
+                                <a href="<?php echo esc_url($permalink); ?>">
+                                    <div class="box_intro box_intro_col">
+                                        <?php if ($thumbnail) : ?>
+                                            <img src="<?php echo esc_url(get_the_post_thumbnail_url($id, 'medium')); ?>" alt="<?php echo esc_attr($ob_title); ?>">
+                                        <?php else : ?>
+                                            <img src="../uploads/default-image.jpg" alt="デフォルト画像">
+                                        <?php endif; ?>
+                                        <h3 class="intro_sbtitle intro_sbtitle_cs"><?php echo esc_html($ob_title); ?></h3>
+                                    </div>
+                                </a>
+                            </li>
+                <?php
+                        endif;
+                    endforeach;
+                endif;
+                ?>
+            </ul>
+        </section>
 
 
 

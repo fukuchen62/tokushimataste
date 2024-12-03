284f9219ae5a4310daae805881a662e44c26ed05
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-03 17:13:01 +0900

    Update single-column.php
    編集中
    
    山口

--- single-column.php
+++ single-column.php
@@ -33,56 +33,38 @@
             </div>
         </section>
 
+
+
         <!-- idと紐づいた情報 -->
         <section>
-            <h3 class="related_all">関連する商品、<br>メーカー様、レシピ</h3>
+            <h3 class="related_all">関連する商品、<br>レシピ、メーカー様</h3>
 
             <ul class="good_used_flex">
-                <!-- 関連する商品 -->
+
                 <li>
-                    <a href="<?php echo get_template_directory_uri(); ?>/html/column_detail.html">
+                    <a href="../html/column_detail.html">
                         <div class="box_intro box_intro_col">
-                            <img src="<?php echo get_template_directory_uri(); ?>/uploads/nikumisoitame.jpg" alt="コラムの写真">
+                            <img src="../uploads/nikumisoitame.jpg" alt="コラムの写真">
                             <h3 class="intro_sbtitle intro_sbtitle_cs">サブタイトルサブタイトル</h3>
                             <!-- <p>このサイトは、徳島県の食文化や特産品、地元で長く親しまれてきた味わいを紹介し、徳島県民や徳島県外の人</p> -->
                         </div>
                     </a>
+
                 </li>
 
-                <!-- メーカー -->
-                <div>
-                    <li>
-                        <a href=" <?php the_permalink(); ?>">
-                            <div class="box_intro box_intro_col">
-
-                                <?php
-                                //カスタムフィールド 'maker_id' に保存された投稿IDを取得
-                                $maker_id = get_field('maker_id');
-
-                                //maker_id から投稿データを取得
-                                $maker_post = get_post($maker_id);
-                                //print_r($maker_post);
-                                if ($maker_post) {
-                                    //投稿タイトル (maker_title) を取得
-                                    $maker_title = $maker_post->post_title;
-                                    //表示
-                                ?>
-                                <?php
-                                    echo '<h3 class="intro_sbtitle intro_sbtitle_cs">メーカー名: ' . esc_html($maker_title) . '</h3>';
-                                    //echo '<p>url: ' . esc_html(the_permalink($maker_id)) . '</p>';
-                                    echo get_post_meta($maker_id, 'address', true);
-                                }
-                                ?>
-                            </div>
-                        </a>
-                    </li>
-                </div>
-
-                <!-- レシピ -->
                 <li>
-                    <a href="<?php echo get_template_directory_uri(); ?>/html/column_detail.html">
+                    <a href="../html/column_detail.html">
                         <div class="box_intro box_intro_col">
-                            <img src="<?php echo get_template_directory_uri(); ?>/uploads/nikumisoitame.jpg" alt="コラムの写真">
+                            <img src="../uploads/nikumisoitame.jpg" alt="コラムの写真">
+                            <h3 class="intro_sbtitle intro_sbtitle_cs">サブタイトルサブタイトル</h3>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="../html/column_detail.html">
+                        <div class="box_intro box_intro_col">
+                            <img src="../uploads/nikumisoitame.jpg" alt="コラムの写真">
                             <h3 class="intro_sbtitle intro_sbtitle_cs">サブタイトルサブタイトル</h3>
                         </div>
                     </a>
@@ -90,7 +72,6 @@
 
             </ul>
         </section>
-
 
 
 

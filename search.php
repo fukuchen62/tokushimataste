95aea7628aa7a5b7ad1f55174b88df11ae61b7fe
q8suk <q8suklip@gmail.com>
2024-12-03 12:25:36 +0900

    Update search.php
    仮
    
    香西

--- search.php
+++ search.php
@@ -148,3 +148,192 @@
 </main>
 <?php /*the_post_thumbnail('medium'); */ ?>
 <?php get_footer(); ?>
+<style>
+    /* 商品名のボタン */
+
+    #goods {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        line-height: 1;
+        text-decoration: none;
+        color: #000000;
+        font-size: 18px;
+        border-radius: 5px;
+        /* width: 250px; */
+        height: 40px;
+        font-weight: bold;
+        border: 2px dashed #000000;
+        transition: 0.3s;
+        background-color: #fbb126;
+
+    }
+
+    #goods:hover {
+        opacity: .5;
+    }
+
+    .box_otomo {
+        border: solid #fbb126;
+        border-radius: 10px;
+        cursor: pointer;
+
+    }
+
+    .card_goods {
+        padding: 0 10px;
+    }
+
+    .pagination {
+        width: 70%;
+        margin: 0 auto 50px;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+    }
+
+    .pagination .number {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+        gap: 5px;
+    }
+
+    .pagination a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        text-decoration: none;
+        cursor: pointer;
+        background-color: #ffffff;
+        border: solid 1px #1597CC;
+        border-radius: 5px;
+        padding: 10px 0;
+    }
+
+    .pagination .number>a.active {
+        background-color: #1597CC;
+        color: #fff;
+    }
+
+    /* ページングで表示するコンテンツのスタイル
+ 初期表示は非表示 */
+    .page_list li {
+        display: none;
+        padding: 20px 0;
+        /* border-bottom: 1px solid #000; */
+        max-width: 320px;
+    }
+
+    /* onクラス添付で画面に表示 */
+    .page_list li.on {
+        display: block;
+    }
+
+    /* ページネーションの現在のページに「.active」クラスを付与して色を変える */
+
+    .pagination .number>a.active {
+        background-color: #1597CC;
+        color: #fff;
+    }
+
+    /* ココから先は見た目のスタイルなのであまり重要ではありません。
+　　自由にデザインなど変更して貰えればと思います。 */
+
+    .page_list {
+        /* border-top: 1px solid #000; */
+        margin-bottom: 20px;
+    }
+
+    .page_list p {
+        margin: 0.3em 0;
+    }
+
+    .pagination {
+        width: 70%;
+        margin: 0 auto 50px;
+        font-size: 16px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+    }
+
+    .pagination .number {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+        gap: 5px;
+    }
+
+    .pagination a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        text-decoration: none;
+        cursor: pointer;
+        background-color: #ffffff;
+        border: solid 1px #1597CC;
+        border-radius: 5px;
+        padding: 10px 0;
+    }
+
+    .img-fluid {
+        width: 320px;
+        height: 240px;
+        border-radius: 8px 8px 0 0;
+    }
+
+    .img-fluid li {
+        letter-spacing: -1em;
+    }
+
+    .img-fluid li {
+        display: inline-block;
+        width: 35px;
+        height: 35px;
+        text-align: center;
+        line-height: 35px;
+        border: 1px solid #FF9900;
+    }
+
+    /*以下変更 byぎみ
+ ページングで表示するコンテンツのスタイル
+ 初期表示は非表示 */
+
+    .page_list {
+        display: flex;
+        list-style: none;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-around;
+        gap: 5px 5px;
+        padding-left: 0;
+        /* ulのユーザーエージェントスタイル打消し */
+    }
+
+    .page_list {
+        /* border-top: 1px solid #000; */
+        margin-bottom: 20px;
+    }
+
+    .page_list li {
+        display: none;
+        padding: 20px 0;
+        /* border-bottom: 1px solid #000; */
+    }
+
+    .page_list li.on {
+        display: block;
+    }
+</style>

6afb36b425e62a2f4cd9cfbba7e4b35404eca426
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-04 12:12:30 +0900

    Update single-recipe.php
    いいね追加
    
    山口

--- single-recipe.php
+++ single-recipe.php
@@ -17,17 +17,21 @@
             ?>
         </h2>
 
-        <!-- ループ外 -->
-        <?php
-        global $wp_query;
-        $post_id = $wp_query->get_queried_object_id();
-        echo get_favorites_button($post_id);
-        ?>
+        <!-- mypageブックマーク用ループ外 -->
+        <div>
+            <?php
+            global $wp_query;
+            $post_id = $wp_query->get_queried_object_id();
+            echo get_favorites_button($post_id);
+            ?>
+        </div>
         <!-- / ループ外 -->
+
         <h2 class="ttl_box">
             <h2 class="ttl_rsp"><?php the_field('recipe_name'); ?></h2>
 
-
+            <!-- いいねボタン -->
+            <?php echo do_shortcode('[wp_ulike]'); ?>
             <!-- スライダー -->
             <div class="container_rs">
                 <div class="slider_rs">
@@ -106,20 +110,7 @@
             </section>
 
             <!-- idに紐づけた商品 -->
-            <!-- <section class="goods_used">
-                <h2>使ったお供</h2>
-                <ul class="flex_use_goods">
-                    <li>
-                        <a href="../html/column_detail.html">
-                            <div class="box_syohin">
-                                <img src="../uploads/nikumisoitame.jpg" alt="コラムの写真">
-                                <h3>サブタイトルサブタイトル</h3>
-                                <p>テキストが入ります。。。テキストは入ります。。。テキストが入ります。。。テキストは入ります。。。テキストが入ります。。。</p>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </section> -->
+
             <section class="rcmd_box">
                 <h3 class="rcmd_b3">関連商品</h3>
                 <ul class="rcmd_good">

63d72a6d10894ba06b739baf953dca29a8f5aa41
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-04 17:06:43 +0900

    Update single-recipe.php
    修正
    
    山口

--- single-recipe.php
+++ single-recipe.php
@@ -26,162 +26,161 @@
             ?>
         </div>
         <!-- / ループ外 -->
+        <h2 class="ttl_rcp"><?php the_field('recipe_name'); ?></h2>
 
-        <h2 class="ttl_box">
-            <h2 class="ttl_rsp"><?php the_field('recipe_name'); ?></h2>
+        <!-- いいねボタン -->
+        <?php echo do_shortcode('[wp_ulike]'); ?>
 
-            <!-- いいねボタン -->
-            <?php echo do_shortcode('[wp_ulike]'); ?>
-            <!-- スライダー -->
-            <div class="container_rs">
-                <div class="slider_rs">
-                    <div>
-                        <?php
-                        $pic = get_field('pic1');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div>
-                        <?php
-                        $pic = get_field('pic2');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div>
-                        <?php
-                        $pic = get_field('pic3');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div>
-                        <?php
-                        $pic = get_field('pic4');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
+        <!-- スライダー -->
+        <div class="container_rs">
+            <div class="slider_rs">
+                <div>
+                    <?php
+                    $pic = get_field('pic1');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
                 </div>
-                <div class="thumbnail_rs">
-                    <div class="thumbnail_rs_img">
-                        <?php
-                        $pic = get_field('pic1');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div class="thumbnail_rs_img">
-                        <?php
-                        $pic = get_field('pic2');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div class="thumbnail_rs_img">
-                        <?php
-                        $pic = get_field('pic3');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
-                    <div class="thumbnail_rs_img">
-                        <?php
-                        $pic = get_field('pic4');
-                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
-                        ?>
-                        <?php if ($pic_url): ?>
-                            <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
-                        <?php endif; ?>
-                    </div>
+                <div>
+                    <?php
+                    $pic = get_field('pic2');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+                <div>
+                    <?php
+                    $pic = get_field('pic3');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+                <div>
+                    <?php
+                    $pic = get_field('pic4');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
                 </div>
             </div>
-
-            <section class="memo_rs">
-                <h2>一言</h2>
-                <div>
-                    <?php the_field('recipe_description'); ?>
-                </div>
-            </section>
-
-            <section class="ingredient_rs">
-                <h2>材料</h2>
-                <div>
-                    <?php the_field('ingredient'); ?>
-                </div>
-            </section>
-
-            <section class="howtomake_rs">
-                <h2>作り方</h2>
-                <div>
-                    <?php the_field('how_to_make'); ?>
-                </div>
-            </section>
-
-            <!-- idに紐づけた商品 -->
-
-            <section class="rcmd_box">
-                <h3 class="rcmd_b3">関連商品</h3>
-                <ul class="rcmd_good">
+            <div class="thumbnail_rs">
+                <div class="thumbnail_rs_img">
                     <?php
-                    // 現在のレシピ投稿のカスタムフィールド 'product_id' を取得
-                    $product_ids = get_field('product_id');
+                    $pic = get_field('pic1');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+                <div class="thumbnail_rs_img">
+                    <?php
+                    $pic = get_field('pic2');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+                <div class="thumbnail_rs_img">
+                    <?php
+                    $pic = get_field('pic3');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+                <div class="thumbnail_rs_img">
+                    <?php
+                    $pic = get_field('pic4');
+                    $pic_url = $pic ? $pic['sizes']['thumbnail'] : '';
+                    ?>
+                    <?php if ($pic_url): ?>
+                        <img src="<?php echo esc_url($pic_url); ?>" alt="<?php the_title_attribute(); ?>" class="img-fluid">
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
 
-                    // $product_ids が配列か単一IDかをチェック
-                    if ($product_ids) {
-                        // 配列でない場合に配列化
-                        $product_ids = is_array($product_ids) ? $product_ids : [$product_ids];
+        <section class="memo_rs">
+            <h2>一言</h2>
+            <div>
+                <?php the_field('recipe_description'); ?>
+            </div>
+        </section>
 
-                        // WP_Queryの設定
-                        $args = [
-                            'post_type' => 'product',        // 商品のカスタム投稿タイプ
-                            'post__in' => $product_ids,     // 指定したIDに基づいて投稿を取得
-                            'posts_per_page' => -1,         // 全件取得
-                        ];
+        <section class="ingredient_rs">
+            <h2>材料</h2>
+            <div>
+                <?php the_field('ingredient'); ?>
+            </div>
+        </section>
 
-                        $the_query = new WP_Query($args);
+        <section class="howtomake_rs">
+            <h2>作り方</h2>
+            <div>
+                <?php the_field('how_to_make'); ?>
+            </div>
+        </section>
 
-                        if ($the_query->have_posts()):
-                            while ($the_query->have_posts()): $the_query->the_post(); ?>
-                                <li>
-                                    <div class="box_syohin">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <!-- アイキャッチ画像 -->
-                                            <?php
-                                            $pic = get_field('pic1');
-                                            $pic_url = $pic ? $pic['sizes']['thumbnail'] : 'path/to/default/image.jpg';
-                                            ?>
-                                            <img src="<?php echo esc_url($pic_url); ?>" alt="関連商品">
-                                            <!-- 商品名 -->
-                                            <h3 class="name_syokuhin"><?php the_title(); ?></h3>
-                                        </a>
-                                    </div>
-                                </li>
-                            <?php endwhile;
-                            wp_reset_postdata();
-                        else: ?>
-                            <p>関連する商品が見つかりませんでした。</p>
-                        <?php endif;
-                    } else { ?>
-                        <p>関連する商品が登録されていません。</p>
-                    <?php } ?>
-                </ul>
-            </section>
+        <!-- idに紐づけた商品 -->
+
+        <section class="rcmd_box"> <!-- クラスなし -->
+            <h3 class="rcmd_b3">関連商品</h3>
+            <ul class="rcmd_good">
+                <?php
+                // 現在のレシピ投稿のカスタムフィールド 'product_id' を取得
+                $product_ids = get_field('product_id');
+
+                // $product_ids が配列か単一IDかをチェック
+                if ($product_ids) {
+                    // 配列でない場合に配列化
+                    $product_ids = is_array($product_ids) ? $product_ids : [$product_ids];
+
+                    // WP_Queryの設定
+                    $args = [
+                        'post_type' => 'product',        // 商品のカスタム投稿タイプ
+                        'post__in' => $product_ids,     // 指定したIDに基づいて投稿を取得
+                        'posts_per_page' => -1,         // 全件取得
+                    ];
+
+                    $the_query = new WP_Query($args);
+
+                    if ($the_query->have_posts()):
+                        while ($the_query->have_posts()): $the_query->the_post(); ?>
+                            <li>
+                                <div class="box_syohin">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <!-- アイキャッチ画像 -->
+                                        <?php
+                                        $pic = get_field('pic1');
+                                        $pic_url = $pic ? $pic['sizes']['thumbnail'] : 'path/to/default/image.jpg';
+                                        ?>
+                                        <img src="<?php echo esc_url($pic_url); ?>" alt="関連商品">
+                                        <!-- 商品名 -->
+                                        <h3 class="name_syokuhin"><?php the_title(); ?></h3>
+                                    </a>
+                                </div>
+                            </li>
+                        <?php endwhile;
+                        wp_reset_postdata();
+                    else: ?>
+                        <p>関連する商品が見つかりませんでした。</p>
+                    <?php endif;
+                } else { ?>
+                    <p>関連する商品が登録されていません。</p>
+                <?php } ?>
+            </ul>
+        </section>
 
 
     </div>

99b86784cc03295ab4ca5573f75b0e7c8774701a
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-06 21:54:35 +0900

    Update index.php
    フードサイエンス消した
    
    山口

--- index.php
+++ index.php
@@ -1,57 +1,17 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 <main>
-    <section class="section">
-        <div class="section_inner">
-            <div class="section_header">
-                <h1 class="heading heading-primary"><span>最新情報</span>NEWS - <?php wp_title(''); ?><?php if (is_year()): ?>年<?php endif; ?></h1>
-            </div>
+    <div class="inner">
+        <!-- パンくずリスト -->
+        <?php get_template_part('template-parts/breadcrumb'); ?>
 
-            <div class="archive">
-                <div class="archive_category">
-                    <h2 class="archive_title">カテゴリー</h2>
-                    <ul class="archive_list">
-                        <?php
-                        $args = [
-                            'title_li' => '', //見出しを削除
-                        ];
-                        wp_list_categories($args);
-                        ?>
-                    </ul>
-                </div>
 
-                <div class="archive_yealy">
-                    <h2 class="archive_title">年別</h2>
-                    <ul class="archive_list">
-                        <?php
-                        $args = [
-                            'type' => 'yearly', //年別を指定
-                        ];
-                        wp_get_archives($args);
-                        ?>
-                    </ul>
-                </div>
-            </div>
+        <ul class="flex">
+            <li>
 
-            <div class="section_body">
-                <?php if (have_posts()): ?>
-                    <div class="cardList">
-                        <?php while (have_posts()): the_post(); ?>
-
-                            <?php get_template_part('template-parts/loop', 'news'); ?>
-
-                        <?php endwhile; ?>
-                    </div>
-                <?php endif; ?>
-
-                <?php if (function_exists('wp_pagenavi')): ?>
-                    <div class="pagination">
-                        <?php wp_pagenavi(); ?>
-                    </div>
-                <?php endif; ?>
-
-            </div>
-
-        </div>
-    </section>
+                <a href="<?php echo home_url('/'); ?>" class="original-button2"><span>TOPへ戻る</span>
+                </a>
+            </li>
+    </div>
 </main>
+
 <?php get_footer(); ?>

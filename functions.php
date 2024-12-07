5f5d4a3aee7bf03e79d862c794e89675517b7fcb
fukuchen62 <fukuchen62@gmail.com>
2024-12-07 14:32:17 +0900

    Update functions.php
    <title>の中身を変更できるように修正
    
    福島

--- functions.php
+++ functions.php
@@ -290,22 +290,22 @@ function add_style_script()
 add_action('wp_enqueue_scripts', 'add_style_script');
 
 /**
- * 検索画面のタイトルをカスタマイズする
+ * 画面の<title>タグをカスタマイズする
  */
 function my_custom_title($title)
 {
     if (is_search()) {
-        // 条件検索ページの場合はタイトルを以下のように変更
-        // tagline（キャッチフレーズ）を無くす
-        // unset($title['tagline']);
-        // タイトルの中身を変更する
-        $title['title'] = '条件検索';
+        // 条件検索ページ
+        $title = '条件検索' . '-' . get_bloginfo('name');
+    } elseif (is_post_type_archive('column') || is_tax('column_type')) {
+        // コラム一覧、コラム詳細ページ
+        $title = 'コラム記事' . '-' . get_bloginfo('name');
     }
     // 変更したタイトルを返す
     return $title;
 }
 // フィルターにフックを追加
-add_filter('document_title_parts', 'my_custom_title');
+add_filter('pre_get_document_title', 'my_custom_title');
 
 
 /**

e83975bccd438dec5b50eaaadcf87d79f4835dc2
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-08 16:13:32 +0900

    コラムにつぶた君入れた
    コラムにつぶた君入れた
    
    山口

--- archive-column.php
+++ archive-column.php
@@ -5,12 +5,11 @@
         <!-- パンくずリスト -->
         <?php get_template_part('template-parts/breadcrumb'); ?>
 
-
-        <!-- 見出し -->
-        <!-- <h2 class="ttl_box">
-            <span class="ttl">コラム</span><br>
-        </h2> -->
-        <!-- コラム無くても良いですか？ -->
+        <div class="layer">
+            <div class="layer-in">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg_column_okome02chara.png" alt="お米キャラクター">
+            </div>
+        </div>
 
         <div class="column_flex">
             <!-- コラム -->

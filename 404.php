584a379381efc20c70ad734e6b6c5a7d7260b3f3
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-08 12:12:52 +0900

    修正
    修正
    
    山口

--- 404.php
+++ 404.php
@@ -3,27 +3,27 @@
     <div class="inner">
         <!-- パンくずリスト -->
         <?php get_template_part('template-parts/breadcrumb'); ?>
+        <div class="inner-sub">
+            <div class="error">
+                <p><span class="marker">404エラー</span></p>
 
-        <div class="error">
-            <p style="text-align:center"><span class="marker">404エラー</span></p>
-            <!-- 登録物がないときは下記の文章を使用します -->
-            <div class="img-none">
-                <img src="" alt="">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404.png" alt="エラー">
-                <!-- style=" margin-top:14px"  -->
+                <div class="img-none">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404.png" alt="エラー">
+                </div>
+                <div class="txt-none">
+                    <p>お探しのページは見つかりませんでした。</p>
+                    <p>URLが間違っているか、お探しのページが存在しません。</p>
+                </div>
+
+                <ul class="flex">
+                    <li>
+
+                        <a href="<?php echo home_url('/'); ?>" class="original-button2"><span>TOPへ戻る</span>
+                        </a>
+                    </li>
             </div>
-            <div class="txt-none">
-                <p>お探しのページは見つかりませんでした。</p>
-                <p>URLが間違っているか、お探しのページが存在しません。</p>
-            </div>
-
-            <ul class="flex">
-                <li>
-
-                    <a href="<?php echo home_url('/'); ?>" class="original-button2"><span>TOPへ戻る</span>
-                    </a>
-                </li>
         </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>

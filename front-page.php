4cfac939f11b1ce75a03b7330813cb640ac93896
a-yamaguchi12 <a.yamaguchi.2nd@gmail.com>
2024-12-09 19:38:09 +0900

    雲南
    山口

--- front-page.php
+++ front-page.php
@@ -96,8 +96,7 @@
                     </a>
                 </div>
                 <div class="kumo minami">
-                    <a href="<?php echo home_url('/area/south/'); ?>">
-                        <img src=" alt=" 県南部">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kumo_minami.png" 県南部">
                     </a>
                 </div>
             </div><?php echo get_template_directory_uri(); ?>/assets/images/kumo_minami.png"
